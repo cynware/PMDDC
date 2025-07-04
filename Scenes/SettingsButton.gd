888b6c5bde96d5668e28e765250cbb537705e4c9
Remukko <bananaz521lol@gmail.com>
2025-07-04 13:48:22 +0200

    settings menuworks!
    ourp

---
+++ Scenes/SettingsButton.gd
@@ -0,0 +1,18 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_settings_pressed():
+	get_node("SettingsMenu").visible = true
+	
+func _on_close_btn_pressed():
+	get_node("SettingsMenu").visible = false
