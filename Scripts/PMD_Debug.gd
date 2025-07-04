65a1845d6dfb33e5d62966194b1ed172d9c952f9
B4D <84340860+bog4d@users.noreply.github.com>
2025-07-04 21:20:39 +0300

    Finished screenshot system methinks

---
+++ Scripts/PMD_Debug.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var initialPosition:Vector2;
+var lerpPosition:Vector2;
+
+var isVisible:bool = true;
+
+func _ready():
+	initialPosition = self.position;
+	
+func _process(delta):
+	position = lerp(position, lerpPosition, 15 * delta);
+	
+	if(Input.is_action_just_pressed("TOGGLE_DEBUG")):
+		isVisible = !isVisible;
+		
+		if(isVisible):
+			lerpPosition = initialPosition
+		else:
+			lerpPosition = initialPosition + Vector2.RIGHT * 800;
+		
--- Scripts/SCREENSHOT_MANAGER.gd
+++ Scripts/SCREENSHOT_MANAGER.gd
@@ -2,11 +2,21 @@ extends Node
 
 
 @onready var saveWindow:FileDialog = $SaveWindow;
+
+@export var transparentBackgroundToggle:CheckBox;
+@export var resolutionMultiplierDropdown:OptionButton;
+
+# so we can hide them for the screenshot
+@export var background:ColorRect;
+@export var debugMenu:Node2D;
+@export var settingsButton:TextureButton;
+ 
 #@export var debugShit:ColorRect;
 
 var capture = null;
 
 func _ready():
+	get_viewport().transparent_bg = true;	
 	DisplayServer.window_set_min_size(Vector2(256, 192));
 	
 func _process(delta):
@@ -16,10 +26,29 @@ func _process(delta):
 		#get_window().mode = Window.MODE_WINDOWED;
 		
 	if Input.is_action_just_pressed("SCREENSHOT"):
-		capture = get_viewport().get_texture().get_image();
-		saveWindow.visible = true;
+		takeTransparentScreenshot();
+		# capture = get_viewport().get_texture().get_image();
 		
 
+func takeTransparentScreenshot():
+	background.visible = !transparentBackgroundToggle.button_pressed;
+	debugMenu.visible = false;
+	settingsButton.visible = false;
+	
+	var viewport := get_viewport()	
+	await RenderingServer.frame_post_draw
+	
+	var img = viewport.get_texture().get_image()
+	var markiplier := int(resolutionMultiplierDropdown.selected)+1;
+	img.resize(256 * markiplier, 192 * markiplier, Image.INTERPOLATE_NEAREST)
+	capture = img;
+	
+	background.visible = true;
+	debugMenu.visible = true;
+	settingsButton.visible = true;
+	
+	saveWindow.visible = true;
+	
 
 func _on_save_window_file_selected(path):
 	print(path);
