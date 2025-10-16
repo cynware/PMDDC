7cb686006357ba9d7aa5a25e888036422267646d
PonPrairie <zoeburgess17@gmail.com>
2025-10-16 21:55:00 +0100

    Debug Tabs
    added new tab swapping, tab animation and tab audio

--- Scripts/PMD_Debug.gd
+++ Scripts/PMD_Debug.gd
@@ -17,3 +17,63 @@ func _process(delta):
 			lerpPosition = initialPosition
 		else:
 			lerpPosition = initialPosition + Vector2.RIGHT * 800;
+
+
+func portrait_tab_pressed():
+	$DebugTab/Tabs/PortraitTab.visible = true
+	$DebugTab/Tabs/DialogueTab.visible = false
+	$DebugTab/Tabs/ThemesTab.visible = false
+	$DebugTab/Tabs/PresetsTab.visible = false
+	$TAB_DISPLAYNAME/Label.text = "PORTRAITS"
+	
+	$PortraitTAB_BTN/ICON.play("CLICK")
+	$ThemesTAB_BTN/ICON.play("IDLE")
+	$PresetsTAB_BTN/ICON.play("IDLE")
+	$DialogueTAB_BTN/ICON.play("IDLE")
+	
+	SoundEffectManager.PlayAccept()
+
+
+func themes_tab_pressed():
+	$DebugTab/Tabs/ThemesTab.visible = true
+	$DebugTab/Tabs/DialogueTab.visible = false
+	$DebugTab/Tabs/PortraitTab.visible = false
+	$DebugTab/Tabs/PresetsTab.visible = false
+	$TAB_DISPLAYNAME/Label.text = "THEMES"
+	
+	$ThemesTAB_BTN/ICON.play("CLICK")
+	$PortraitTAB_BTN/ICON.play("IDLE")
+	$PresetsTAB_BTN/ICON.play("IDLE")
+	$DialogueTAB_BTN/ICON.play("IDLE")
+	
+	SoundEffectManager.PlayAccept()
+
+
+func presets_tab_pressed():
+	$DebugTab/Tabs/PresetsTab.visible = true
+	$DebugTab/Tabs/DialogueTab.visible = false
+	$DebugTab/Tabs/ThemesTab.visible = false
+	$DebugTab/Tabs/PortraitTab.visible = false
+	$TAB_DISPLAYNAME/Label.text = "PRESETS"
+	
+	$PresetsTAB_BTN/ICON.play("CLICK")
+	$DialogueTAB_BTN/ICON.play("IDLE")
+	$ThemesTAB_BTN/ICON.play("IDLE")
+	$PortraitTAB_BTN/ICON.play("IDLE")
+	
+	SoundEffectManager.PlayAccept()
+
+
+func dialogue_tab_pressed():
+	$DebugTab/Tabs/DialogueTab.visible = true
+	$DebugTab/Tabs/ThemesTab.visible = false
+	$DebugTab/Tabs/PortraitTab.visible = false
+	$DebugTab/Tabs/PresetsTab.visible = false
+	$TAB_DISPLAYNAME/Label.text = "DIALOGUE"
+	
+	$DialogueTAB_BTN/ICON.play("CLICK")
+	$PresetsTAB_BTN/ICON.play("IDLE")
+	$ThemesTAB_BTN/ICON.play("IDLE")
+	$PortraitTAB_BTN/ICON.play("IDLE")
+	
+	SoundEffectManager.PlayAccept()
