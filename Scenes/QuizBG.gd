d5058e86070eec4b5007c1c7e7ebb8fc83412c1b
Remukko <bananaz521lol@gmail.com>
2025-10-28 12:20:49 +0100

    fix yo shit

--- Scenes/QuizBG.gd
+++ Scenes/QuizBG.gd
@@ -1,18 +1,12 @@
 extends SubViewportContainer
 
-var frame_count := 0
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var speed := 30.0 # pixels per second
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	frame_count += 1
-	if frame_count % 2 == 0:
-		$QuizViewport/Layer_1.position.x += 1
-		$QuizViewport/Layer_2.position.x -= 1
-		# print($QuizViewport/Layer_1.position.x)
+	$QuizViewport/Layer_1.position.x += speed * delta
+	$QuizViewport/Layer_2.position.x -= speed * delta
+
 	if $QuizViewport/Layer_1.position.x >= 192:
 		$QuizViewport/Layer_1.position.x = -192
 		$QuizViewport/Layer_2.position.x = 192
+
