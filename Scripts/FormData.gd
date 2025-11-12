c205c09b8678d69350591c53a14d05cf6fa8e5d0
Cyn <84340860+cynware@users.noreply.github.com>
2025-11-13 01:09:16 +0200

    pmdCollab json parsing

--- Scripts/FormData.gd
+++ Scripts/FormData.gd
@@ -1,23 +1,31 @@
+# FormData.gd
 extends Resource
 class_name FormData
 
-var name: String
-var bot_path: String
-var complete: int
-var filename: String
-var portraits: Array = []
-var preversed
+var name: String = ""
+var bot_path: String = ""
+var complete: int = 0
+var filename: String = ""
+var portraits: PackedInt32Array = PackedInt32Array()
+var preversed: PackedInt32Array = PackedInt32Array()
 var credits: Array = []
-var modified: String
+var modified: String = ""
 
 static func from_json(form_json: Dictionary) -> FormData:
-	var f = FormData.new()
+	var f := FormData.new()
 	f.name = form_json.get("name", "")
 	f.bot_path = form_json.get("botPath", "")
-	f.complete = form_json.get("complete", 0)
+	f.complete = int(form_json.get("complete", 0))
 	f.filename = form_json.get("filename", "")
-	f.portraits = form_json.get("portraits", [])
-	f.preversed = form_json.get("preversed", [])
+
+	var portraits_data = form_json.get("portraits", [])
+	if portraits_data is Array:
+		f.portraits = PackedInt32Array(portraits_data)
+
+	var preversed_data = form_json.get("preversed", [])
+	if preversed_data is Array:
+		f.preversed = PackedInt32Array(preversed_data)
+
 	f.credits = form_json.get("credits", [])
 	f.modified = form_json.get("modified", "")
 	return f
